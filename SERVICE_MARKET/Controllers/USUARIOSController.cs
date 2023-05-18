7d9fc705b58314ca6c15e781013ae1b57d2be74f
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-05-18 00:06:00 -0500

    Controlador USUARIOS
    - Cadena de conexión
    - Lista del objeto
    - Método para encriptar y desencriptar contraseña

---
+++ SERVICE_MARKET/Controllers/USUARIOSController.cs
@@ -0,0 +1,44 @@
+﻿using SERVICE_MARKET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SERVICE_MARKET.Controllers
+{
+    public class USUARIOSController : Controller
+    {
+        /*CADENA DE CONEXION*/
+        private static string conexion = "Data Source=LAPTOP-RMAAM810;Initial Catalog=SERVICE_MARKET;Integrated Security=True";
+
+        /*LISTA DE OBJETO*/
+        private static List<multipleModel> model = new List<multipleModel>();
+
+        /*-----------------------------------------------------------------------------------------------------------------------*/
+
+        /*METODO PARA ENCRIPTAR CONTRASEÑA*/
+        public static string Encriptar(string texto)
+        {
+            string result = string.Empty;
+            byte[] encryted =
+            System.Text.Encoding.Unicode.GetBytes(texto);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+
+        /*METODO PARA DESENCRIPTAR CONTRASEÑA*/
+        public static string DesEncriptar(string texto)
+        {
+            string result = string.Empty;
+            byte[] decryted =
+            Convert.FromBase64String(texto);
+            //result = 
+            System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+            result = System.Text.Encoding.Unicode.GetString(decryted);
+            return result;
+        }
+
+        /*-----------------------------------------------------------------------------------------------------------------------*/
+    }
+}
