3d9cce102bf400428a9d174b9400a4ae6d37b2a1
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-05-18 00:32:42 -0500

    Cadena de conexión y objeto en el controlador HOME

---
+++ SERVICE_MARKET/Controllers/HOMEController.cs
@@ -0,0 +1,21 @@
+﻿using SERVICE_MARKET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SERVICE_MARKET.Controllers
+{
+    public class HOMEController : Controller
+    {
+        /*CADENA DE CONEXION*/
+        private static string conexion = "Data Source=LAPTOP-RMAAM810;Initial Catalog=SERVICE_MARKET;Integrated Security=True";
+
+        /*LISTA DE OBJETO*/
+        private static List<multipleModel> model = new List<multipleModel>();
+
+        /*-----------------------------------------------------------------------------------------------------------------------*/
+
+    }
+}
