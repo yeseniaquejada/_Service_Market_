abb6cc8634a708cab684149d81b36384fee3f1b4
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-05-18 00:07:20 -0500

    Método para registrar usuarios

--- SERVICE_MARKET/Controllers/USUARIOSController.cs
+++ SERVICE_MARKET/Controllers/USUARIOSController.cs
@@ -1,6 +1,8 @@
 ﻿using SERVICE_MARKET.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -40,5 +42,68 @@ namespace SERVICE_MARKET.Controllers
         }
 
         /*-----------------------------------------------------------------------------------------------------------------------*/
+
+        /*METODO PARA REGISTRAR USUARIOS*/
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(multipleModel oUsuarios)
+        {
+            bool registrado;
+            string mensaje;
+
+            /*COMPARANDO CONTRASEÑAS*/
+            if (oUsuarios.CONTRASENA_USU == oUsuarios.CONFIRMAR_CONTRASENA_U)
+            {
+                /*ENCRIPTANDO CONTRASEÑA*/
+                oUsuarios.CONTRASENA_USU = Encriptar(oUsuarios.CONTRASENA_USU);
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Las contraseñas no coinciden";
+                return View();
+            }
+
+            /*CONECTANDO BASE DE DATOS*/
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                /*PROCEDIMIENTO ALMACENADO REGISTRAR USUARIO*/
+                SqlCommand cmd = new SqlCommand("REGISTRAR_USUARIOS", cn);
+                cmd.Parameters.AddWithValue("NOMBRE_COMPLETO_USU", oUsuarios.NOMBRE_COMPLETO_USU);
+                cmd.Parameters.AddWithValue("CELULAR_USU", "57" + oUsuarios.CELULAR_USU);
+                cmd.Parameters.AddWithValue("ID_CIUDAD_FK", oUsuarios.ID_CIUDAD_FK);
+                cmd.Parameters.AddWithValue("CORREO_ELECTRONICO_USU", oUsuarios.CORREO_ELECTRONICO_USU);
+                cmd.Parameters.AddWithValue("CONTRASENA_USU", oUsuarios.CONTRASENA_USU);
+                cmd.Parameters.Add("REGISTRADO", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("MENSAJE", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                /*LEER PARAMETROS DE SALIDA*/
+                registrado = Convert.ToBoolean(cmd.Parameters["REGISTRADO"].Value);
+                mensaje = cmd.Parameters["MENSAJE"].Value.ToString();
+
+            }
+
+            ViewData["MENSAJE"] = mensaje;
+
+            if (registrado)
+            {
+                return RedirectToAction("Login", "USUARIOS");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        /*-----------------------------------------------------------------------------------------------------------------------*/
+
+
     }
 }
