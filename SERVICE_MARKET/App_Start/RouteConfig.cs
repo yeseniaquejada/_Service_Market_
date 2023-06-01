972ea8ae99e13089e784dba786d6782627419be1
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-05-31 22:22:23 -0500

    RouteConfig pagina de inicio y sesión

--- SERVICE_MARKET/App_Start/RouteConfig.cs
+++ SERVICE_MARKET/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SERVICE_MARKET
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HOME", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
