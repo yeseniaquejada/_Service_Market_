f8347376cec06a93d79de7eb60cd7b9a8a03104b
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-06-03 16:14:53 -0500

    Manejo de error en el METODO PARA CONSULTAR PUBLICACIONES Y SOLICITUDES DE SERVICIOS (HOME)

--- SERVICE_MARKET/Controllers/HOMEController.cs
+++ SERVICE_MARKET/Controllers/HOMEController.cs
@@ -22,41 +22,48 @@ namespace SERVICE_MARKET.Controllers
         /*METODO PARA CONSULTAR PUBLICACIONES Y SOLICITUDES DE SERVICIOS*/
         public ActionResult Index(int pagina = 1, int elementosPorPagina = 12)
         {
-            List<multipleModel> model = new List<multipleModel>();
+            try {
+                List<multipleModel> model = new List<multipleModel>();
 
-            using (SqlConnection oconexion = new SqlConnection(conexion))
-            {
-                SqlCommand Comand = new SqlCommand("CONSULTAR_SERVICIOS", oconexion);
-                Comand.CommandType = CommandType.StoredProcedure;
-                oconexion.Open();
-
-                using (SqlDataReader dr = Comand.ExecuteReader())
+                using (SqlConnection oconexion = new SqlConnection(conexion))
                 {
-                    while (dr.Read())
-                    {
-                        multipleModel oServicios = new multipleModel();
-                        oServicios.ID_SERVICIO = Convert.ToInt32(dr["ID_SERVICIO"]);
-                        oServicios.NOMBRE_SER = dr["NOMBRE_SER"].ToString();
-                        oServicios.PRECIO_SER = decimal.Parse(dr["PRECIO_SER"].ToString());
-                        oServicios.DESCRIPCION_BREVE = dr["DESCRIPCION_BREVE"].ToString();
-                        oServicios.TIPO = dr["TIPO"].ToString();
-                        oServicios.NOMBRE_CAT = dr["NOMBRE_CAT"].ToString();
-                        model.Add(oServicios);
+                    SqlCommand Comand = new SqlCommand("CONSULTAR_SERVICIOS", oconexion);
+                    Comand.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
 
+                    using (SqlDataReader dr = Comand.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            multipleModel oServicios = new multipleModel();
+                            oServicios.ID_SERVICIO = Convert.ToInt32(dr["ID_SERVICIO"]);
+                            oServicios.NOMBRE_SER = dr["NOMBRE_SER"].ToString();
+                            oServicios.PRECIO_SER = decimal.Parse(dr["PRECIO_SER"].ToString());
+                            oServicios.DESCRIPCION_BREVE = dr["DESCRIPCION_BREVE"].ToString();
+                            oServicios.TIPO = dr["TIPO"].ToString();
+                            oServicios.NOMBRE_CAT = dr["NOMBRE_CAT"].ToString();
+                            model.Add(oServicios);
+
+                        }
                     }
                 }
+                /*Calcular los índices de inicio y fin para la página actual*/
+                int indiceInicio = (pagina - 1) * elementosPorPagina;
+                int indiceFin = indiceInicio + elementosPorPagina;
+
+                /*Obtener la lista de servicios para la página actual*/
+                List<multipleModel> serviciosPagina = model.Skip(indiceInicio).Take(elementosPorPagina).ToList();
+
+                ViewBag.TotalPaginas = (int)Math.Ceiling((double)model.Count / elementosPorPagina);
+                ViewBag.PaginaActual = pagina;
+
+                return View(serviciosPagina);
+            } 
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Se produjo un error al consultar las publicaciones y solicitudes de servicios.";
+                return View("Index");
             }
-            /*Calcular los índices de inicio y fin para la página actual*/
-            int indiceInicio = (pagina - 1) * elementosPorPagina;
-            int indiceFin = indiceInicio + elementosPorPagina;
-
-            /*Obtener la lista de servicios para la página actual*/
-            List<multipleModel> serviciosPagina = model.Skip(indiceInicio).Take(elementosPorPagina).ToList();
-
-            ViewBag.TotalPaginas = (int)Math.Ceiling((double)model.Count / elementosPorPagina);
-            ViewBag.PaginaActual = pagina;
-
-            return View(serviciosPagina);
         }
 
         /*-----------------------------------------------------------------------------------------------------------------------*/
